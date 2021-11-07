4379dc47ae6104398752deaf3401e963ae83f417
omartinez@domalabs.com <omartinez@domalabs.com>
2021-11-07 11:08:54 -0500

    changed gae

--- stable_baselines3/common/buffers.py
+++ stable_baselines3/common/buffers.py
@@ -369,7 +369,7 @@ class RolloutBuffer(BaseBuffer):
         # Convert to numpy
         last_values = last_values.clone().cpu().numpy().flatten()
 
-        last_gae_lam = 0
+        
         for step in reversed(range(self.buffer_size)):
             if step == self.buffer_size - 1:
                 next_non_terminal = 1.0 - dones
@@ -377,13 +377,13 @@ class RolloutBuffer(BaseBuffer):
             else:
                 next_non_terminal = 1.0 - self.episode_starts[step + 1]
                 next_values = self.values[step + 1]
-            self.gae_lambda = 0
-            delta = self.rewards[step] + self.gamma * next_values * self.values[step]
-            last_gae_lam = delta + self.gamma * self.gae_lambda * next_non_terminal * last_gae_lam
+          
+            delta = self.rewards[step] + self.gamma * next_non_terminal * self.values[step]
+            last_gae_lam = delta - self.values[step]
             self.advantages[step] = last_gae_lam
         # TD(lambda) estimator, see Github PR #375 or "Telescoping in TD(lambda)"
         # in David Silver Lecture 4: https://www.youtube.com/watch?v=PnHCvfgC_ZA
-        self.returns = self.advantages + self.values
+        self.returns = self.advantages
 
 
     def compute_returns_and_advantage(self, last_values: th.Tensor, dones: np.ndarray) -> None:
