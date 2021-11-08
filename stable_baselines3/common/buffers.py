7ecc92549441e83e0c60aa7e77f4d048a92f75c1
omartinez@domalabs.com <omartinez@domalabs.com>
2021-11-08 11:12:15 -0500

    Changes to the compute rewards function

--- stable_baselines3/common/buffers.py
+++ stable_baselines3/common/buffers.py
@@ -348,20 +348,7 @@ class RolloutBuffer(BaseBuffer):
 
     def compute_returns_nstep(self, last_values: th.Tensor, dones: np.ndarray) -> None:
         """
-        Post-processing step: compute the lambda-return (TD(lambda) estimate)
-        and GAE(lambda) advantage.
-
-        Uses Generalized Advantage Estimation (https://arxiv.org/abs/1506.02438)
-        to compute the advantage. To obtain vanilla advantage (A(s) = R - V(S))
-        where R is the discounted reward with value bootstrap,
-        set ``gae_lambda=1.0`` during initialization.
-
-        The TD(lambda) estimator has also two special cases:
-        - TD(1) is Monte-Carlo estimate (sum of discounted rewards)
-        - TD(0) is one-step estimate with bootstrapping (r_t + gamma * v(s_{t+1}))
-
-        For more information, see discussion in https://github.com/DLR-RM/stable-baselines3/pull/375.
-
+        Uses the advantage function estimate A^pi(s,a) but using n_steps as opposed to just one.
         :param last_values: state value estimation for the last step (one for each env)
         :param dones: if the last step was a terminal step (one bool for each env).
 
@@ -378,7 +365,7 @@ class RolloutBuffer(BaseBuffer):
                 next_non_terminal = 1.0 - self.episode_starts[step + 1]
                 next_values = self.values[step + 1]
           
-            delta = self.rewards[step] + self.gamma * delta * next_values
+            delta = self.rewards[step] + self.gamma * delta * (next_non_terminal+next_values)
             last_gae_lam = delta - self.values[step]
             self.advantages[step] = last_gae_lam
         # TD(lambda) estimator, see Github PR #375 or "Telescoping in TD(lambda)"
