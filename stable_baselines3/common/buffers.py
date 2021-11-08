cb2221f78399f5065fc45641353564931c6301d6
omartinez@domalabs.com <omartinez@domalabs.com>
2021-11-08 10:52:14 -0500

    Compute rewards modifications

--- stable_baselines3/common/buffers.py
+++ stable_baselines3/common/buffers.py
@@ -369,7 +369,7 @@ class RolloutBuffer(BaseBuffer):
         # Convert to numpy
         last_values = last_values.clone().cpu().numpy().flatten()
 
-        delta = 1
+        #delta = 1
         for step in reversed(range(self.buffer_size)):
             if step == self.buffer_size - 1:
                 next_non_terminal = 1.0 - dones
@@ -378,7 +378,7 @@ class RolloutBuffer(BaseBuffer):
                 next_non_terminal = 1.0 - self.episode_starts[step + 1]
                 next_values = self.values[step + 1]
           
-            delta = self.rewards[step] + self.gamma * delta * next_non_terminal
+            delta = self.rewards[step] + self.gamma * next_values
             last_gae_lam = delta - self.values[step]
             self.advantages[step] = last_gae_lam
         # TD(lambda) estimator, see Github PR #375 or "Telescoping in TD(lambda)"
